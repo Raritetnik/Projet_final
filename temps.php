6d6dccd5098a2abb05006936ba13898e2a6edc37
Raritetnik <mkodarenko@gmail.com>
2022-05-12 21:09:45 -0400

    Ajout de code sur les pages

--- temps.php
+++ temps.php
@@ -13,7 +13,38 @@
         <h1>Horloge</h1>
     </header>
     <main>
+    <?php
+    date_default_timezone_set('Canada/Eastern');
 
+
+    $hour = intval(date("h"));
+    $min = intval(date("i"));
+
+
+    echo "<table border='1px' cellpadding = '2'>";
+    echo "<tr> <th></th>";
+    for($j = 0; $j < 60; $j++) {
+        echo "<th>$j</th>";
+    }
+    echo "</th>";
+
+    for($i = 1; $i <= 12; $i++)
+    {
+        echo "<tr><th>$i</th>";
+        for($j = 0; $j < 60; $j++)
+        {
+            if($i == $hour && $j == $min)
+            {
+                echo "<td  align='middle' style='background-color: #fff'> X </td>";
+            }
+            else
+            {
+                echo "<td></td>";
+            }
+        }
+            echo "</tr>";
+    }
+    ?>
     </main>
 </body>
 </html>
