7753e6d7823afddef4e112b2551fbc34b8120fe0
Raritetnik <mkodarenko@gmail.com>
2022-05-16 05:21:37 -0700

    FIX soumission horloge

--- temps.php
+++ temps.php
@@ -15,7 +15,7 @@
     <main>
         <form method="get">
             <h3>Deviner l'heure:
-                <input type="text" name="heureUtilisateur">
+                <input type="time" name="heureUtilisateur">
                 <input type="submit" value='Soumettre'></h2>
         </form>
     <?php
@@ -24,43 +24,43 @@
     $min = intval(date("i"));
 
     if (isset($_GET['heureUtilisateur'])) {
-        $pattern = '/^([1-9]|1[0-2]):[0-5][0-9]$/';
+        $pattern = '/^(0[1-9]|1[0-2]):[0-5][0-9]$/';
         $heureUtilisateur = '';
         $heureUtilisateur = $_GET['heureUtilisateur'];
-
         $heure = explode(':',$heureUtilisateur);
     }
 
     if(isset($_GET['heureUtilisateur']) && !empty($heureUtilisateur)) {
         if (!preg_match($pattern, $heureUtilisateur)) {
-            echo '<h3>L\'heure indiqué ne correspond pas au bon format: [heures 1 à 12:minutes 0 à 59]! Exemple: 4:23 </h3>';
-        }
-        echo "<table class='tabHeures' border='1px' cellpadding = '2'>";
-        for($i = 1; $i <= 12; $i++)
-        {
-            for($j = 0; $j < 60; $j++)
+            echo '<h3>L\'heure indiqué '.$heureUtilisateur.' ne correspond pas au bon format: [heures 1 à 12:minutes 0 à 59]! Exemple: 4:23 am </h3>';
+        } else {
+            echo "<table class='tabHeures' border='1px' cellpadding = '2'>";
+            for($i = 1; $i <= 12; $i++)
             {
-                if($i == $hour && $j == $min)
+                for($j = 0; $j < 60; $j++)
                 {
-                    if(preg_match($pattern, $heureUtilisateur)
-                    && $heure[0] == $i && $heure[1] == $j){
-                        echo "<td  align='middle' style='background-color: green'>";
-                        echo ($j <= 9) ? "$i:0$j</td>" : "$i:$j</td>";
-                    } else {
-                        echo "<td  align='middle' style='background-color: aqua'>";
+                    if($i == $hour && $j == $min)
+                    {
+                        if(preg_match($pattern, $heureUtilisateur)
+                        && $heure[0] == $i && $heure[1] == $j){
+                            echo "<td  align='middle' style='background-color: green'>";
+                            echo ($j <= 9) ? "$i:0$j</td>" : "$i:$j</td>";
+                        } else {
+                            echo "<td  align='middle' style='background-color: aqua'>";
+                            echo ($j <= 9) ? "$i:0$j</td>" : "$i:$j</td>";
+                        }
+                    }
+                    else if ($heure[0] == $i && $heure[1] == $j) {
+                        echo "<td  align='middle' style='background-color: red'>";
                         echo ($j <= 9) ? "$i:0$j</td>" : "$i:$j</td>";
                     }
+                    else
+                    {
+                        echo ($j <= 9) ? "<td>$i:0$j</td>" : "<td>$i:$j</td>";
+                    }
                 }
-                else if ($heure[0] == $i && $heure[1] == $j) {
-                    echo "<td  align='middle' style='background-color: red'>";
-                    echo ($j <= 9) ? "$i:0$j</td>" : "$i:$j</td>";
-                }
-                else
-                {
-                    echo ($j <= 9) ? "<td>$i:0$j</td>" : "<td>$i:$j</td>";
-                }
+                    echo "</tr>";
             }
-                echo "</tr>";
         }
     }
     ?>
